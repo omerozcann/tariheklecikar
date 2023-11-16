8b2ebe2be0e2ed4dbb538131431d20e764e53c51
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-11-16 14:42:54 +0300

    .

---
+++ tariheklecikar/tariheklecikar/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace tariheklecikar
+{
+    partial class tarihecFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.hesaplabtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.farktxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(97, 39);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Başlangıç";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bitiş";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(97, 67);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // hesaplabtn
+            // 
+            this.hesaplabtn.Location = new System.Drawing.Point(321, 41);
+            this.hesaplabtn.Name = "hesaplabtn";
+            this.hesaplabtn.Size = new System.Drawing.Size(75, 23);
+            this.hesaplabtn.TabIndex = 4;
+            this.hesaplabtn.Text = "Hesapla";
+            this.hesaplabtn.UseVisualStyleBackColor = true;
+            this.hesaplabtn.Click += new System.EventHandler(this.hesaplabtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fark";
+            // 
+            // farktxt
+            // 
+            this.farktxt.Location = new System.Drawing.Point(97, 120);
+            this.farktxt.Name = "farktxt";
+            this.farktxt.Size = new System.Drawing.Size(100, 22);
+            this.farktxt.TabIndex = 6;
+            // 
+            // tarihecFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 165);
+            this.Controls.Add(this.farktxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.hesaplabtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "tarihecFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tarihler Arası Farkı Bul";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button hesaplabtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox farktxt;
+    }
+}
+
---
+++ tariheklecikar/tariheklecikar/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tariheklecikar
+{
+    public partial class tarihecFrm : Form
+    {
+        public tarihecFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void hesaplabtn_Click(object sender, EventArgs e)
+        {            
+            DateTime t1=new DateTime(dateTimePicker1.Value.Year, dateTimePicker1.Value.Month, dateTimePicker1.Value.Day);
+            DateTime t2 = new DateTime(dateTimePicker2.Value.Year, dateTimePicker2.Value.Month, dateTimePicker2.Value.Day);
+            var sonuc = t2 - t1;
+            farktxt.Text= sonuc.ToString();
+        }
+    }
+}
